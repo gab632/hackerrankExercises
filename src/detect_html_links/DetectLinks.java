b44627e2671f227686408ca74ed4a3fd25bcc625
Gabriel O. <gabriele.kkj@gmail.com>
2025-06-13 19:47:38 +0200

    HeartsMindsSolution Java file added.

--- src/detect_html_links/DetectLinks.java
+++ src/detect_html_links/DetectLinks.java
@@ -8,78 +8,40 @@ public class DetectLinks {
 
     public static void main(String[] args) throws IOException {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//        scanner.nextLine();
-        File file = new File("src/detect_html_links/testCases01DetectLinks.txt");
-        File testCases05 = new File("src/detect_html_links/testCases-05-DetectLinks.txt");
-
-        File expectedResults05 = new File("src/detect_html_links/expectedOutput-05-DetectLinks.txt");
-
-        BufferedReader reader = new BufferedReader(new FileReader(testCases05));
-        BufferedReader resultsReader = new BufferedReader(new FileReader(expectedResults05));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
         Pattern linkPattern = Pattern.compile("<a href=\"[\\S]+\"");
-        Pattern endingTagPattern = Pattern.compile(">.*</.*>");
-        // Pattern contentPattern = Pattern.compile(">.+</a>");
+        Pattern endingTagPattern = Pattern.compile(">[.[^<>]]*</.*>");
 
         Matcher linkMatcher;
         Matcher endTagMatcher;
 
         String line = "";
-        String expectedLine = "";
+        for(int i = 0; i < n; i++){
+            line = scanner.nextLine();
 
-        while((line = reader.readLine()) != null) {
-//            line = reader.readLine();
             String link = "";
             String content = "";
 
-            /*
-            <li style="-moz-float-edge: content-box">... that
-            <a href="/wiki/Orval_Overall" title="Orval Overall">Orval Overall</a>
-            <i>(pictured)</i> is the only <b>
-            <a href="/wiki/List_of_Major_League_Baseball_pitchers_who_have_struck_out_four_batters_in_one_inning" title="List of Major League Baseball pitchers who have struck out four batters in one inning">Major League Baseball player to strike out four batters in one inning</a></b> in the
-            <a href="/wiki/World_Series" title="World Series">World Series</a>?</li>
-             */
-
             if (line.contains("<a href=\"")) {
-                expectedLine = resultsReader.readLine();
+
                 linkMatcher = linkPattern.matcher(line);
 
                 while(linkMatcher.find()) {
 
                     link = linkMatcher.group().substring(9, linkMatcher.group().length() - 1);
+
                     endTagMatcher = endingTagPattern.matcher(line.substring(linkMatcher.start()));
                     endTagMatcher.find();
-//                    System.out.println("test: " + linkMatcher.start() + " - " + endTagMatcher.group());
-                    content = endTagMatcher.group().substring(1, endTagMatcher.group().indexOf('<'));
-                    String resultLine = String.format("%s,%s", link, content);
-//                    System.out.printf("%s,%s\n", link, content);
-//                    System.out.println(resultLine);
-                    if(!resultLine.equals(expectedLine)){
-                        System.out.println("result: " + resultLine + " - exp.: " + expectedLine);
-                    }
-//                    StringBuilder sb = new StringBuilder(line);
+                    content = endTagMatcher.group().substring(1, endTagMatcher.group().indexOf("</")).trim();
 
-//                    while (endTagMatcher.find()) {
-//                        // String endTag = m.group();
-////                        System.out.println(endTagMatcher.group());
-////                        System.out.println();
-//                        sb.delete(endTagMatcher.start(), endTagMatcher.end());
-//                    }
+                    System.out.printf("%s,%s\n", link, content);
 
-
-//                    content = sb.toString();
-////                    System.out.println(content);
-//                    content = content.substring(content.lastIndexOf(">") + 1);
-
-                    //                System.out.println("test: " + content);
-                    //                System.out.println(content.lastIndexOf(">" + 1);
-                    //                System.out.println("Test 2: " + content);
                 }
             }
-
-        }// while
-//            scanner.close();
+        }// for-loop
+        scanner.close();
     }
 }
---
+++ src/hearts_minds_challenge/HeartsMindsSolution.java
@@ -0,0 +1,106 @@
+package hearts_minds_challenge;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/*
+https://www.wearedevelopers.com/
+https://puzzles.code100.dev/puzzles/hearts-and-minds
+* find out the following details:
+How many of the hearts ❤️ and how many of the minds 🧠 are straight?
+“straight” means they point fully up, down, left or right at a straight angle
+How many minds 🧠 are on the left side of the bunch?
+How many hearts ❤️ are on the right side of the bunch?
+*/
+public class HeartsMindsSolution {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        InputStream in = new URI("https://puzzles.code100.dev/code100/puzzles/hearts-and-minds/hearts-and-minds.html").toURL().openStream();
+        byte[] bytes = in.readAllBytes();
+        String input = new String(bytes);
+        String[] lines = input.split("\\n");
+
+        /*
+        * result[0] = Straight hearts,
+        * result[1] = Straight minds,
+        * result[0] = Minds to the left side of the screen,
+        * result[0] = Minds to the right side of the screen.
+        */
+        int[] result = countStraightOnes(lines);
+
+        // If you want to print the result to stout:
+        printResult(result);
+
+        in.close();
+    }
+
+    public static int[] countStraightOnes(String[] input) {
+        int hearts = 0;
+        int minds = 0;
+        int mindsOnTheLeft = 0;
+        int heartsOnTheRight = 0;
+
+        for(String line : input){
+
+            if(hasMind(line)){
+                if(isStraight(line)){
+                    minds++;
+                }
+                if(isOnTheLeft(line)){
+                    mindsOnTheLeft++;
+                }
+            }
+            if(hasHeart(line)){
+                if(isStraight(line)){
+                    hearts++;
+                }
+                if(isOnTheRight(line)){
+                    heartsOnTheRight++;
+                }
+            }
+
+        }
+        return new int[]{hearts, minds, mindsOnTheLeft, heartsOnTheRight};
+    }
+    // helper I
+    public static boolean hasHeart(String line){
+        return line.contains("\u2665");
+    }
+
+    // helper II
+    public static boolean hasMind(String line){
+        return line.contains("\uD83E\uDDE0");
+    }
+
+    // helper III
+    public static boolean isStraight(String line){
+        return line.contains("rot-90")
+                || line.contains("rot-180")
+                || line.contains("rot-270")
+                || line.contains("rot-0");
+    }
+
+    // helper IV
+    // I assumed the middle value is 50.
+    public static boolean isOnTheLeft(String line){
+        int value = Integer.parseInt(line.substring(line.indexOf("left-") + 5, line.indexOf("left-") + 7));
+        return value < 50;
+    }
+
+    // helper V
+    // I assumed the middle value is 50.
+    public static boolean isOnTheRight(String line){
+        int value = Integer.parseInt(line.substring(line.indexOf("left-") + 5, line.indexOf("left-") + 7));
+        return value > 50;
+    }
+
+    // helper VI
+    public static void printResult(int[] input){
+        System.out.println("\nStraight hearts: " + input[0]
+                + "\nStraight minds: " + input[1]
+                + "\nMinds to the left: " + input[2]
+                + "\nHearts to the right: " + input[3]);
+    }
+}
