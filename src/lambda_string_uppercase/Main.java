b357207243e0e008f06729b44405e454caaebeaf
Gabriel O. <gabriele.kkj@gmail.com>
2025-07-08 20:27:53 +0200

    two-lists lambda exercise solved.

--- src/lambda_string_uppercase/Main.java
+++ src/lambda_string_uppercase/Main.java
@@ -1,8 +1,8 @@
 package lambda_string_uppercase;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 import java.util.function.Function;
 
 public class Main {
@@ -35,18 +35,61 @@ public class Main {
         strings.forEach(System.out::println);
 
         System.out.println("""
-                Write a Java program to create a lambda that simultaneously returns two lists: 
+                Write a Java program to create a lambda that simultaneously returns two lists:
                 one with uppercase and one with lowercase versions.
-                
                 """);
+        List<String> testList = List.of("tESt 1", "TESt 2", "TesT 3", "tesT 4");
 //        Function<List<String>, List<List<String>>> toListsFunction =
+        Function<String, String> stringToUpperCase = s -> s.toUpperCase();
+        Function<String, String> stringToLowerCase = s -> s.toLowerCase();
+        Function<List<String>, List<String>> listToUpperCase = stringList
+                -> stringList
+                .stream()
+                .map(stringToUpperCase)
+                .toList();
+        Function<List<String>, List<String>> listToLowerCase = stringList
+                -> stringList.stream()
+                .map(String::toLowerCase)
+                .toList();
+        System.out.println(listToLowerCase.apply(testList));
+        System.out.println(listToUpperCase.apply(testList));
 
+        Function<List<String>, Map<String, List<String>>> listBothUppercaseAndLowercase = (stringList) -> {
 
+            List<String> uppr = listToUpperCase.apply(stringList);
+            List<String> lwr = listToLowerCase.apply(stringList);
 
+            Map<String, List<String>> listMap = new HashMap<>();
+            listMap.put("UPPERCASE", uppr);
+            listMap.put("LOWERACSE", lwr);
+            return listMap;
+        };
 
+        Function<List<String>, List<List<String>>> nestedListsBothUppercaseAndLowercase = (stringList) -> List.of(listToUpperCase.apply(stringList), listToLowerCase.apply(stringList));
 
+        Function<List<String>, List<List<String>>> nestedListsSimplified = (stringList) ->
+                List.of(
+                        stringList
+                            .stream()
+                            .map(s -> s.toUpperCase())
+                            .toList(),
+                        stringList
+                                .stream()
+                                .map(s -> s.toLowerCase())
+                                .toList()
+                );
+        System.out.println("\nUsing stream() map() toList():");
+        nestedListsSimplified.apply(testList).forEach(System.out::println);
+
+        System.out.println("""
+                Write a Java program to implement a lambda 
+                that maps a list of mixed-case strings 
+                to a list of alternating uppercase and lowercase letters.
+                """);
 
 
 
     }
+
+
 }
